7d7f9b4be9e14b4949d38d87e141cccd305e2d4d
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-01 23:17:48 +0000

    survey

--- survey.js
+++ survey.js
@@ -8,30 +8,25 @@ const rl = readline.createInterface({
 rl.question('What do you think of Node.js? ', (answer) => {
     console.log(`Thank you for your valuable feedback: ${answer}`);
 
-    rl.close();
+    rl.question("What's your name? ", (answer) => {
+        console.log(`Thank you for your valuable feedback: ${answer}`);
 
-rl.question("What's your name? ", (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+        rl.question("What's an activity you like doing? ", (answer) => {
+            console.log(`Thank you for your valuable feedback: ${answer}`);
 
-    rl.close();
+            rl.question("What do you listen to while doing that? ", (answer) => {
+                console.log(`Thank you for your valuable feedback: ${answer}`);
 
-rl.question("What's an activity you like doing? ", (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+                rl.question("What's your favourite meal? ", (answer) => {
+                    console.log(`Thank you for your valuable feedback: ${answer}`);
 
-    rl.close();
+                    rl.question("Which sport is your absolute favourite? ", (answer) => {
+                        console.log(`Thank you for your valuable feedback: ${answer}`);
 
-rl.question("What do you listen to while doing that? ", (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-
-    rl.close();
-
-rl.question("What's your favourite meal? ", (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-
-    rl.close();
-
-rl.question("Which sport is your absolute favourite? ", (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-
-    rl.close();
+                        rl.close();
+                    });
+                });
+            });
+        });
+    });
 });
